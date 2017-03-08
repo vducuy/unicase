c30993dbe1a608c25f00a45d9c58898b2cec09ee
root <root@localhost.localdomain>
2017-03-08 17:04:37 +0700

    Work version remove Orderby Combobox

--- unicase-child/functions.php
+++ unicase-child/functions.php
@@ -8,4 +8,3 @@
 /**
  * Include all your custom code here
  */
-remove_action( ‘woocommerce_before_shop_loop’, ‘woocommerce_catalog_ordering’, 30 );
--- unicase/functions.php
+++ unicase/functions.php
@@ -14,3 +14,4 @@ require get_template_directory() . '/inc/init.php';
  * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
  * http://codex.wordpress.org/Child_Themes
  */
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 15 );
